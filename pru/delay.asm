ddf72943e44c4ea00d1cc5cb0f7dddc338e388f4
Steven Hopkins <yobe-server@Mac.hsd1.nh.comcast.net>
2025-12-06 21:41:32 -0500

    external assembly delay function fix

--- pru/delay.asm
+++ pru/delay.asm
@@ -5,7 +5,7 @@
 
 delay_cycles:
     ; R14 contains the cycle count parameter
-loop:
+delay_loop:
     SUB  R14, R14, 1
-    QBNE loop, R14, 0
+    QBNE delay_loop, R14, 0
     JMP  R3.w2              ; Return (R3.w2 contains return address)
